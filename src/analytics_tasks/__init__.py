8b72e5f9e55dcf7a20f1609c0d84b8ab8488d94b
ashugaur <ashutoshgaur@live.com>
2025-06-22 11:19:18 +0530

    Structure changes

--- src/analytics_tasks/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Analytics Tasks - A collection of data analysis utilities."""
-
-__version__ = "0.1.0"
-
-# Import commonly used functions/classes for easy access
-from .file_search.functions import *
--- src/analytics_tasks/file_search/__init__.py
+++
@@ -1,8 +0,0 @@
-"""File search utilities."""
-
-from .build import *
-from .functions import *
-from .load_search_polars import *
-
-# Make key functions available at package level
-# __all__ = ['search_function', 'another_function', 'build_index']
